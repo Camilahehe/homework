3d8d2c5ec2d95fe5a4109f760faf91e638c58cad
Camilahehe <91740147+Camilahehe@users.noreply.github.com>
2021-10-30 18:11:00 +0300

    Add files via upload

---
+++ практика несчастной камилы дз 2.cpp
@@ -0,0 +1,32 @@
+﻿// ConsoleApplication4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//домашка 2
+
+#include <iostream>
+#include <ctime>
+    using namespace std;
+
+     int main()
+     {
+         srand(time(NULL));
+         int size;
+         cin >> size;
+         int* arr = new int[size];
+         for (int i = 0; i < size ; i++)
+         {
+         hehe:
+             arr[i] = rand() % size;
+             for (int j=0;j<i;j++)
+             {
+                 if (arr[j] == arr[i])
+                     goto hehe;
+             }
+
+
+         }
+         for (int i = 0; i < size ; i++)
+             cout << arr[i] << " ";
+
+
+     }
+
+
